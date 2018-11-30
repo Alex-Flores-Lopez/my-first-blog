b8d9f4c00069b7351f8643322ea9a92a6fafa1b1
Alex <alexielxdd@gmail.com>
2018-11-30 09:21:22 -0400

    mi aplicacion

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'AlexFloresLopez$prueba',
+        'USER': 'AlexFloresLopez',
+        'PASSWORD': '*master007*',
+        'HOST': 'AlexFloresLopez.mysql.pythonanywhere-services.com',
     }
 }
 
